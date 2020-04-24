dccd9667c4170b397e06cd22f1b0765c2b687777
MIVI <mickael.vilalard@hotmail.fr>
2020-04-24 19:12:03 +0200

    Delete Configuration Salle

--- src/main/esgi/ddd/model/salle/Salle.java
+++ src/main/esgi/ddd/model/salle/Salle.java
@@ -1,11 +1,8 @@
 package main.esgi.ddd.model.salle;
 
 import main.esgi.ddd.common.entity.Entity;
-import main.esgi.ddd.common.valueObjectId.ValueObjectId;
-import main.esgi.ddd.model.configurationSalle.ConfigurationSalle;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Objects;
 
@@ -13,23 +10,16 @@ public class Salle extends Entity {
 
     private final String nom;
 
-    private final ConfigurationSalle configurationSalle;
-
     private final HashSet<LocalDate> disponibilites;
 
-    public Salle(String nom, ConfigurationSalle configurationSalle, HashSet<LocalDate> disponibilites) {
+    public Salle(String nom, HashSet<LocalDate> disponibilites) {
         super(new SalleID());
         this.nom = nom;
-        this.configurationSalle = configurationSalle;
         this.disponibilites = disponibilites;
     }
 
     public String getNom() {
         return nom;
-    }
-
-    public ConfigurationSalle getConfigurationSalle() {
-        return configurationSalle;
     }
 
     public HashSet<LocalDate> getDisponibilites() {
